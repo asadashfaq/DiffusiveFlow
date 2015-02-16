8c2a30e779b8f3467f5f6c84bb9438dc0f4a5ffe
Bo Tranberg <bo@tberg.dk>
2015-02-16 12:02:58 +0100

    node power mixes

--- plotting.py
+++ plotting.py
@@ -6,22 +6,44 @@ import matplotlib.pyplot as plt
 Plotting comparison of diffusive flow and up/down stream approach
 """
 
+names = ['AT', 'FI', 'NL', 'BA', 'FR', 'NO', 'BE', 'GB', 'PL', 'BG', 'GR',\
+         'PT', 'CH', 'HR', 'RO', 'CZ', 'HU', 'RS', 'DE', 'IE', 'SE', 'DK',\
+         'IT', 'SI', 'ES', 'LU', 'SK', 'EE', 'LV', 'LT']
+
+phi = np.load('./data/phi.npy')
 pm = np.load('./results/power_mix_node_import_100.npy')
 dpm = np.load('./results/dpm.npy')
+np.fill_diagonal(dpm,0)
 
 nodes = pm.shape[0]
 shift = .3
 width = .5
 
-plt.figure()
-plt.subplot(1,2,1)
-plt.bar(range(nodes), dpm[0,:], width, edgecolor='none', color='SteelBlue')
-plt.bar(np.arange(shift,nodes+shift,1), pm[0,:], width, edgecolor='none', color='LightSteelBlue')
-plt.xticks(np.arange((width+shift)*.5,nodes+(width+shift)*.5,1), ('0AT', '1FI', '2NL', '3BA', '4FR', '5NO','6BE', '7GB', '8PL', '9BG', '10GR','11PT', '12CH', '13HR', '14RO', '15CZ','16HU', '17RS', '18DE', '19IE', '20SE','21DK', '22IT', '23SI', '24ES', '25LU','26SK', '27EE', '28LV', '29LT'),rotation=75 )
+def plotNodes(t=100):
+    """
+    Function that plots nodes' power mixes to compare diffusive flow to up/down
+    stream approach.
+    """
+    for n in range(nodes):
+        plt.figure(figsize=(14,7))
+        plt.subplot(1,2,1)
+        plt.bar(range(nodes), dpm[n,:], width, edgecolor='none', color='SteelBlue')
+        plt.bar(np.arange(shift,nodes+shift,1), pm[n,:], width, edgecolor='none', color='LightSteelBlue')
+        plt.xticks(np.arange((width+shift)*.5,nodes+(width+shift)*.5,1), names, rotation=75, fontsize=10)
+        plt.legend(('Diffusive flow','Up- down stream'), loc='best')
+        plt.title('Import', fontsize=12)
+        plt.ylabel('MW')
+        plt.ylim(ymin=0)
 
-plt.subplot(1,2,2)
-plt.bar(range(nodes), dpm[:,0], width, edgecolor='none', color='SteelBlue')
-plt.bar(np.arange(shift,nodes+shift,1), pm[:,0], width, edgecolor='none', color='LightSteelBlue')
-plt.xticks(np.arange((width+shift)*.5,nodes+(width+shift)*.5,1), ('0AT', '1FI', '2NL', '3BA', '4FR', '5NO','6BE', '7GB', '8PL', '9BG', '10GR','11PT', '12CH', '13HR', '14RO', '15CZ','16HU', '17RS', '18DE', '19IE', '20SE','21DK', '22IT', '23SI', '24ES', '25LU','26SK', '27EE', '28LV', '29LT'),rotation=75 )
+        plt.subplot(1,2,2)
+        plt.bar(range(nodes), dpm[:,n], width, edgecolor='none', color='SteelBlue')
+        plt.bar(np.arange(shift,nodes+shift,1), pm[:,n], width, edgecolor='none', color='LightSteelBlue')
+        plt.xticks(np.arange((width+shift)*.5,nodes+(width+shift)*.5,1), names, rotation=75, fontsize=10)
+        plt.legend(('Diffusive flow','Up- down stream'), loc='best')
+        plt.title('Export', fontsize=12)
+        plt.ylabel('MW')
+        plt.ylim(ymin=0)
 
-plt.show()
+        plt.suptitle(names[n] + " (" + str(int(round(phi[n,t]))) + "), t = "+str(t), fontsize=14)
+
+        plt.savefig('./figures/nodes/'+str(n)+'.png', bbox_inches='tight')
