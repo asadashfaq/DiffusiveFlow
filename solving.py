488d2deb4769868096f6651e3dcee2818c9d3a76
Bo Tranberg <bo@tberg.dk>
2015-02-17 15:13:46 +0100

    reducing amount of data

--- solving.py
+++ solving.py
@@ -22,6 +22,7 @@ def solver(startPoint):
     """
     for t in range(int(startPoint), int(startPoint+interval)):
         i, pf, ip, lf, pn = diffusiveIterator(A, K, phi, t, frac=1, direction='export', objective=.1)
+        lf = np.sum(lf, 0)
         np.savez(savePath+str(t)+'_o_'+str(.1)+'.npz', i=i, powerFrac=pf, linkFlow=lf, powerMix=pn)
 
 if 'timeseries' in task:
