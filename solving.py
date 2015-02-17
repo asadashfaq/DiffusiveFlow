9e966ca39832918a597227ac5bf84d376274c9ab
Bo Tranberg <bo@tberg.dk>
2015-02-17 16:29:35 +0100

    fractional sink solver mode

--- solving.py
+++ solving.py
@@ -6,24 +6,50 @@ from multiprocessing import Pool
 from diffusive import diffusiveIterator
 
 """
-DOCSTRING
+Different ways of calling the diffusive iterator to solve the power flows and
+colormixes for different time steps and fractional constraints on the sinks.
 """
 
 """
 Initialisation
 """
-#if len(sys.argv) < 2:
-#    raise Exception('Not enough inputs!')
-#else:
-task = str(sys.argv[:])
+if len(sys.argv) < 2:
+    raise Exception('Not enough inputs!')
+else:
+    task = str(sys.argv[:])
 
 def solver(startPoint):
     """
+    Solve a range of time steps for a fixed fraction
     """
     for t in range(int(startPoint), int(startPoint+interval)):
         i, pf, ip, lf, pn = diffusiveIterator(A, K, phi, t, frac=1, direction='export', objective=.1)
         lf = np.sum(lf, 0)
         np.savez(savePath+str(t)+'_o_'+str(.1)+'.npz', i=i, powerFrac=pf, linkFlow=lf, powerMix=pn)
+
+def fractionSolver(timeStep, fractions):
+    """
+    Solve a single time step for a range of fractions
+    """
+    for fraction in fractions:
+        i, pf, ip, lf, pn = diffusiveIterator(A, K, phi, timeStep, frac=fraction, direction='export', objective=.1)
+        lf = np.sum(lf, 0)
+        np.savez(savePath+'t_'+str(t)+'_f_'+str(fraction)+'_o_'+str(.1)+'.npz', i=i, powerFrac=pf, linkFlow=lf, powerMix=pn)
+
+if 'fraction' in task:
+    """
+    Run the diffusive iterator on a single time step for different values of
+    the fractional constraint on the sinks
+    """
+    print "Solving fractional sinks"
+    A = np.loadtxt('./settings/Europeadmat.txt')
+    K = np.load('data/K.npy')
+    phi = np.load('./data/phi.npy')
+    savePath = './results/fraction/'
+    fractions = np.linspace(.1,1,10)
+    timeSteps = [0, 100, 1000, 5000, 20000]
+    for t in timeSteps:
+        fractionSolver(t, fractions)
 
 if 'timeseries' in task:
     """
