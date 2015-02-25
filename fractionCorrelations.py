284a0a8c93f0c1a44018186e7d823c2e78426837
Bo Tranberg <bo@tberg.dk>
2015-02-25 15:49:46 +0100

    better structure

--- fractionCorrelations.py
+++ fractionCorrelations.py
@@ -6,7 +6,13 @@ import matplotlib.pyplot as plt
 
 """
 Investigating correlation of power mixes between diffusive flow and up/down
-stream approach for different fraction constraints on the sinks
+stream approach for different fraction constraints on the sinks.
+
+Ways of calling:
+plot:   plot colormesh of correlation of power mixes for different fractions
+
+example:
+python fractionCorrelation.py plot load
 """
 
 """
@@ -53,65 +59,66 @@ else:
     names = names
     title = ''
 
-for t in timeSteps:
-    t = int(t)
-    # Load power mixes from up/down stream approach
-    phi = np.load('./data/phi.npy')
-    pm = np.load('./input/linear_pm.npz', mmap_mode='r')
-    pmex = pm['power_mix_ex'][:,:,t]
-    pmim = pm['power_mix'][:,:,t]
-    nodes = pmim.shape[0]
+if 'plot' in task:
+    for t in timeSteps:
+        t = int(t)
+        # Load power mixes from up/down stream approach
+        phi = np.load('./data/phi.npy')
+        pm = np.load('./input/linear_pm.npz', mmap_mode='r')
+        pmex = pm['power_mix_ex'][:,:,t]
+        pmim = pm['power_mix'][:,:,t]
+        nodes = pmim.shape[0]
 
-    # Remove self import/export from up/down stream approach
-    for n in range(nodes):
-        if phi[n,t] > 0:
-            pmim[n,n] = 0
-        if phi[n,t] < 0:
-            pmex[n,n] = 0
+        # Remove self import/export from up/down stream approach
+        for n in range(nodes):
+            if phi[n,t] > 0:
+                pmim[n,n] = 0
+            if phi[n,t] < 0:
+                pmex[n,n] = 0
 
-    # Load power mixes from diffusive iterator
-    iteration = np.zeros(len(fractions))
-    initPower = np.zeros(len(fractions))
-    powerFrac = np.zeros(len(fractions))
-    linkFlow = np.zeros((len(fractions),50,30))
-    powerMix = np.zeros((len(fractions),30,30))
+        # Load power mixes from diffusive iterator
+        iteration = np.zeros(len(fractions))
+        initPower = np.zeros(len(fractions))
+        powerFrac = np.zeros(len(fractions))
+        linkFlow = np.zeros((len(fractions),50,30))
+        powerMix = np.zeros((len(fractions),30,30))
 
-    for i,f in enumerate(fractions):
-        d = np.load('./results/fraction/t_'+str(t)+'_f_'+str(f)+'_o_0.1.npz')
-        iteration[i] = d['i']
-        initPower[i] = d['initPower']
-        powerFrac[i] = d['powerFrac']
-        linkFlow[i] = d['linkFlow']
-        pm = d['powerMix']
-        np.fill_diagonal(pm, 0)
-        powerMix[i] = pm
+        for i,f in enumerate(fractions):
+            d = np.load('./results/fraction/t_'+str(t)+'_f_'+str(f)+'_o_0.1.npz')
+            iteration[i] = d['i']
+            initPower[i] = d['initPower']
+            powerFrac[i] = d['powerFrac']
+            linkFlow[i] = d['linkFlow']
+            pm = d['powerMix']
+            np.fill_diagonal(pm, 0)
+            powerMix[i] = pm
 
-    # compare power mixes from the two approaches
-    # if node is a sink compare imports, if node is a source compare exports
-    corr = np.zeros((nodes, len(fractions)))
-    newNames = np.copy(names)
-    for n in range(nodes):
-        if round(phi[order[n],t],4) == 0:
-            newNames[n] = names[n]
-            for i in range(len(fractions)):
-                corr[n,i] = 0
-        if round(phi[order[n],t],4) > 0:
-            newNames[n] = '+ '+names[n]
-            for i in range(len(fractions)):
-                corr[n,i] = pearsonr(pmex[order[n]], powerMix[i,:,order[n]])[0]
-        if round(phi[order[n],t],4) < 0:
-            newNames[n] = '- '+names[n]
-            for i in range(len(fractions)):
-                corr[n,i] = pearsonr(pmim[order[n]], powerMix[i,order[n],:])[0]
+        # compare power mixes from the two approaches
+        # if node is a sink compare imports, if node is a source compare exports
+        corr = np.zeros((nodes, len(fractions)))
+        newNames = np.copy(names)
+        for n in range(nodes):
+            if round(phi[order[n],t],4) == 0:
+                newNames[n] = names[n]
+                for i in range(len(fractions)):
+                    corr[n,i] = 0
+            if round(phi[order[n],t],4) > 0:
+                newNames[n] = '+ '+names[n]
+                for i in range(len(fractions)):
+                    corr[n,i] = pearsonr(pmex[order[n]], powerMix[i,:,order[n]])[0]
+            if round(phi[order[n],t],4) < 0:
+                newNames[n] = '- '+names[n]
+                for i in range(len(fractions)):
+                    corr[n,i] = pearsonr(pmim[order[n]], powerMix[i,order[n],:])[0]
 
 
-    plt.figure()
-    ax = plt.subplot()
-    plt.pcolormesh(corr)
-    plt.colorbar()
-    ax.set_xticks(np.linspace(.5,9.5,10))
-    ax.set_xticklabels(np.linspace(.1,1,10))
-    ax.set_yticks(np.linspace(.5,29.5,30))
-    ax.set_yticklabels(newNames,ha="right",va="center",fontsize=8)
-    plt.xlabel('fraction')
-    plt.savefig('./figures/power_corr/t_'+str(t)+title+'.png', bbox_inches='tight')
+        plt.figure()
+        ax = plt.subplot()
+        plt.pcolormesh(corr)
+        plt.colorbar()
+        ax.set_xticks(np.linspace(.5,9.5,10))
+        ax.set_xticklabels(np.linspace(.1,1,10))
+        ax.set_yticks(np.linspace(.5,29.5,30))
+        ax.set_yticklabels(newNames,ha="right",va="center",fontsize=8)
+        plt.xlabel('fraction')
+        plt.savefig('./figures/fraction correlation/t_'+str(t)+title+'.png', bbox_inches='tight')
